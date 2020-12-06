b7d552ce9c3a5996a3d061541dca928869a62944
jpcarosuarez <46928724+jpcarosuarez@users.noreply.github.com>
2020-12-06 18:22:15 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Kiosko Inmobiliario
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Encuentra tu Hogar
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
