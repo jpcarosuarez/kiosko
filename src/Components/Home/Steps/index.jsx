d54cbe6c32d91cd4373b04ac9e36de89e2e07775
jpcarosuarez <46928724+jpcarosuarez@users.noreply.github.com>
2021-03-12 18:57:57 -0500

    paso a paso

--- src/Components/Home/Steps/index.jsx
+++ src/Components/Home/Steps/index.jsx
@@ -1,6 +1,11 @@
 import {memo} from 'react';
-import {Link} from 'react-router-dom';
 import './steps.css';
+import { BiBuildingHouse } from 'react-icons/bi';
+import { FaTruckMoving} from 'react-icons/fa';
+
+import {TiThumbsUp} from 'react-icons/ti';
+
+
 
 const ComoFunciona = () => {
     console.log('ComoFunciona');
@@ -8,27 +13,26 @@ const ComoFunciona = () => {
     return (
         <section className="comoFunciona">
 
-            <h1>Cómo Funciona</h1>
-
-            
+            <h1>Arrienda en simples pasos</h1>
             <div className="steps">
                 <div className="step1">
-
-                    <Link to="">
-                        Encuentra un espacio
-                    </Link>
+                    <div className="circulo">
+                        <h2>Encuentra el espacio que deseas utilizar</h2>
+                    </div>
+                    <h3><BiBuildingHouse /></h3>
                 </div>
                 <div className="step2">
-                    <Link to="">
-                        Ház una reservacion
-                    </Link>
+                    <div className="circulo">
+                        <h2>Aprueba el estudio o paga al contado</h2>
+                    </div>
+                    <h3><TiThumbsUp /></h3>
                 </div>
                 <div className="step3">
-                    <Link to="">
-                        Mudate al espacio  
-                    </Link>
+                    <div className="circulo">
+                        <h2>Ya puedes utilizar el espacio</h2>
+                    </div>
+                    <h3><FaTruckMoving /></h3>
                 </div>
-
             </div>
         </section>
     )
